559b7cc55a0d26fd4531a71c452307b5fa1bcc50
Michael Kane <mk@mk4p.com>
2025-01-16 15:02:38 -0500

    Styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import {defineConfig} from 'astro/config';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: 'static',
+  integrations: [
+    tailwind(),
+    starlight({
+      title: 'Cerebruh',
+      description: '2nd Brain Infrastructure',
+      social: {
+        github: 'https://github.com/bigpager/cerebruh.is',
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          items: [
+            { label: 'Introduction', link: '/getting-started/introduction/' },
+            { label: 'Installation', link: '/getting-started/installation/' },
+          ],
+        },
+      ],
+    }),
+  ],
 });
